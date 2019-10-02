ce6d997d1f92bca21628596705212fe470d2232e
Zach <fireknight95@gmail.com>
2019-10-02 02:16:56 -0400

    add global var

--- api-tests.py
+++ api-tests.py
@@ -1,34 +1,8 @@
 from app import app, db, Cards
-import unittest
 import json
 
-#GET Request tests
-def test_GetCardInfo():
-    response = app.test_client().get('/api/v1/cards/1') #TODO create temperary record to enure test coverage
-    json_data = json.loads(response.data)
-
-    assert response.status_code == 200
-    assert 'card' in json_data
-    assert 'code' in json_data
-    assert 'id' in json_data['card']
-    assert 'name' in json_data['card']
-    assert 'color' in json_data['card']
-    assert 'cost' in json_data['card']
-    assert 'type' in json_data['card']
-    assert 'rarity' in json_data['card']
-def test_GetCardDoesNotExist():
-    response = app.test_client().get('/api/v1/cards/9999')
-    json_data = json.loads(response.data)
-
-    assert response.status_code == 404
-    assert 'error' in json_data
-    assert 'code' in json_data['error']
-    assert 'message' in json_data['error']
-def test_GetCardBadInput():
-    response = app.test_client().get('/api/v1/cards/one')
-
-    assert response.status_code == 404
-
+#TODO restructure to use JSON schema files for testing
+card_id = 0
 
 #POST Request tests
 def test_PostCardInfo():
@@ -45,6 +19,8 @@ def test_PostCardInfo():
     )
 
     json_data = json.loads(response.data)
+    global card_id 
+    card_id = json_data['card']['id']
 
     assert response.status_code == 201
     assert 'card' in json_data
@@ -55,6 +31,7 @@ def test_PostCardInfo():
     assert 'cost' in json_data['card']
     assert 'type' in json_data['card']
     assert 'rarity' in json_data['card']
+
 def test_PostCardMissingField():
     response = app.test_client().post(
     '/api/v1/cards',
@@ -72,6 +49,7 @@ def test_PostCardMissingField():
     assert 'error' in json_data
     assert 'code' in json_data['error']
     assert 'message' in json_data['error']
+
 def test_PostCardBadInput():
     response = app.test_client().post(
     '/api/v1/cards',
@@ -90,12 +68,45 @@ def test_PostCardBadInput():
     assert 'error' in json_data
     assert 'code' in json_data['error']
     assert 'message' in json_data['error']
+
+#GET Request tests
+def test_GetCardInfo():
+    global card_id
+    response = app.test_client().get('/api/v1/cards/' + str(card_id))
+    json_data = json.loads(response.data)
+
+    assert response.status_code == 200
+    assert 'card' in json_data
+    assert 'code' in json_data
+    assert 'id' in json_data['card']
+    assert 'name' in json_data['card']
+    assert 'color' in json_data['card']
+    assert 'cost' in json_data['card']
+    assert 'type' in json_data['card']
+    assert 'rarity' in json_data['card']
+
+def test_GetCardDoesNotExist():
+    response = app.test_client().get('/api/v1/cards/9999')
+    json_data = json.loads(response.data)
+
+    assert response.status_code == 404
+    assert 'error' in json_data
+    assert 'code' in json_data['error']
+    assert 'message' in json_data['error']
+
+def test_GetCardBadInput():
+    response = app.test_client().get('/api/v1/cards/one')
+
+    assert response.status_code == 404
+
+
     
 
 #PUT Request test
 def test_PutCardInfo():
+    global card_id
     response = app.test_client().put(
-    '/api/v1/cards/1',
+    '/api/v1/cards/' + str(card_id),
     data=json.dumps({
         "name": "guildpact informant",
         "type": "creature",
@@ -120,8 +131,9 @@ def test_PutCardInfo():
 
 #DELETE Request test
 def test_DeleteCardInfo():
+    global card_id
     response = app.test_client().delete(
-    '/api/v1/cards/1',
+    '/api/v1/cards/' + str(card_id),
     data=json.dumps({
         "name": "guildpact informant",
         "type": "creature",
